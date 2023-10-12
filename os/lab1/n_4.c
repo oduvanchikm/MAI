95c10e06fc0d179d0de63c1b69ef8058721ad1aa
Mira <mkgubareva2005@gmail.com>
2023-10-13 00:59:41 +0300

    new

--- os/lab1/n_4.c
+++ os/lab1/n_4.c
@@ -1,60 +1,72 @@
 #include <stdio.h>
 #include <string.h>
-
-#define ARRAY_SIZE 128
-
+#include <stdlib.h>
+#include <stdbool.h>
 
 enum status_code
 {
     ok,
     error_filename,
     error_with_open_file,
-    error_with_writing_file,
-    error_with_reading_file,
     invalid_flag,
 };
 
-
-int flag_xor8(FILE* file, int* array)
+int flag_xor8(FILE* file)
 {
     long long sum = 0;
-    char byte;
+    unsigned char byte;
 
-    while ((byte = fread(&byte, sizeof(char), 1, file)) > 0)
+    while (fread(&byte, sizeof(unsigned char), 1, file) > 0)
     {
         sum ^= byte;
-        // printf("%lld\n", sum);
-        array[0] += sum;
     }
 
-    return ok;
+    return sum;
 }
 
-int flag_xor32(FILE* file, int* array)
+int flag_xor32(FILE* file)
 {
-    long long sum = 0;
-    int byte;
-
-    while ((byte = fread(&byte, sizeof(int), 1, file)) > 0)
-    {
-        sum ^= byte;
-        // printf("%lld\n", sum);
-        array[0] += sum;
+    unsigned int bytes;
+    long int result = 0;
+    while(fread(&bytes, sizeof(unsigned int), 1, file) > 0)
+    {   
+        result ^= bytes;
     }
-
-    return ok;
+    return result;
 }
 
-// int mask()
+int to_ten(char *number, int base)
+{
+    int result = 0;
+    char *ptr = number;
+
+    while (*ptr)
+    {
+        result = result * base + (*ptr > '9' ? *ptr - 'A' + 10 : *ptr - '0');
+        ptr++;
+    }
+
+    return result;
+}
+
+int flag_mask(FILE* file, char* argv)
+{
+    int count;
+    unsigned int mask_array;
+    long int result = 0;
+    unsigned long int mask = to_ten(argv, 16);
+    while(fread(&mask, sizeof(unsigned int), 1, file) > 0)
+    {   
+        if(memcmp(mask_array, &mask, 4) == 0)
+        {
+            result += 1;
+        }
+    }
+    return result;
+}
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3 && argc != 4)
-    {
-        printf("Error\n");
-        return error_filename;
-    }
-
 
     FILE* file = fopen(argv[1], "rb");
     if (file == NULL)
@@ -65,29 +77,29 @@ int main(int argc, char* argv[])
 
     if (strcmp(argv[2], "xor8") == 0)
     {
-        int array[ARRAY_SIZE] = {};
-        flag_xor8(file, array);
-        printf("Result xor8: %d\n", array[0]);
+        int res = flag_xor8(file);
+        printf("Result xor8: %d\n", res);
     }
-
     else if (strcmp(argv[2], "xor32") == 0)
     {
-        int array[ARRAY_SIZE] = {};
-        flag_xor32(file, array);
-        printf("Result xor 32: %d\n", array[0]);
+        int res = flag_xor32(file);
+        printf("Result xor32: %d\n", res);
     }
-
     else if (strcmp(argv[2], "mask") == 0)
     {
-        printf("bebe");
-    }
 
+        long long res = flag_mask(file, argv[3]);
+
+
+        printf("%d\n", res);
+        
+    }
     else 
     {
         printf("Invalid flag\n");
         return invalid_flag;
     }
 
+    fclose(file);
     return ok;
-
 }
