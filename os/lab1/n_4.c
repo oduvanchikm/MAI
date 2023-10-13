4739f2477dcf2be474d9f58f46acf4e6ec877d4f
Mira <mkgubareva2005@gmail.com>
2023-10-13 03:18:29 +0300

    final res

--- os/lab1/n_4.c
+++ os/lab1/n_4.c
@@ -11,10 +11,10 @@ enum status_code
     invalid_flag,
 };
 
-int flag_xor8(FILE* file)
+unsigned char flag_xor8(FILE* file)
 {
-    long long sum = 0;
-    unsigned char byte;
+    unsigned char sum = 0;
+    unsigned char byte = 0;
 
     while (fread(&byte, sizeof(unsigned char), 1, file) > 0)
     {
@@ -24,14 +24,16 @@ int flag_xor8(FILE* file)
     return sum;
 }
 
-int flag_xor32(FILE* file)
+unsigned int flag_xor32(FILE* file)
 {
-    unsigned int bytes;
-    long int result = 0;
+    unsigned int bytes = 0;
+    unsigned int result = 0;
+
     while(fread(&bytes, sizeof(unsigned int), 1, file) > 0)
-    {   
+    {
         result ^= bytes;
     }
+
     return result;
 }
 
@@ -49,57 +51,70 @@ int to_ten(char *number, int base)
     return result;
 }
 
-int flag_mask(FILE* file, char* argv)
+unsigned int flag_mask(FILE* file, char* argv)
 {
-    int count;
-    unsigned int mask_array;
-    long int result = 0;
-    unsigned long int mask = to_ten(argv, 16);
-    while(fread(&mask, sizeof(unsigned int), 1, file) > 0)
-    {   
-        if(memcmp(mask_array, &mask, 4) == 0)
-        {
-            result += 1;
-        }
+    unsigned int number;
+    unsigned int result = 0;
+    unsigned int mask = to_ten(argv, 16);
+
+    while(fread(&number, sizeof(unsigned int), 1, file) > 0)
+    {
+        result += (number & mask) != 0 ? 1 : 0;
     }
+
     return result;
 }
 
 int main(int argc, char* argv[])
 {
-
     FILE* file = fopen(argv[1], "rb");
+
     if (file == NULL)
     {
         printf("Error with open file\n");
+
         return error_with_open_file;
     }
 
-    if (strcmp(argv[2], "xor8") == 0)
-    {
-        int res = flag_xor8(file);
-        printf("Result xor8: %d\n", res);
-    }
-    else if (strcmp(argv[2], "xor32") == 0)
-    {
-        int res = flag_xor32(file);
-        printf("Result xor32: %d\n", res);
-    }
-    else if (strcmp(argv[2], "mask") == 0)
-    {
 
-        long long res = flag_mask(file, argv[3]);
-
-
-        printf("%d\n", res);
-        
-    }
-    else 
+    if (argc == 3)
     {
+        if (strcmp(argv[2], "xor8") == 0)
+        {
+            printf("Result xor8: %d\n", flag_xor8(file));
+        }
+
+        else if (strcmp(argv[2], "xor32") == 0)
+        {
+            printf("Result xor32: %d\n", flag_xor32(file));
+        }
+    }
+
+    else if (argc == 4)
+    {
+        if (strcmp(argv[2], "mask") == 0)
+        {
+            printf("%d\n", flag_mask(file, argv[3]));
+        }
+
+        else
+        {
+            printf("Invalid flag\n");
+            fclose(file);
+
+            return invalid_flag;
+        }
+    }
+
+    else {
         printf("Invalid flag\n");
+        fclose(file);
+
         return invalid_flag;
+        
     }
 
     fclose(file);
+
     return ok;
 }
