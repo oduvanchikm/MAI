f7f50d4a1615a3b44935fd91b18751e831c6f75e
Mira <mkgubareva2005@gmail.com>
2023-10-26 04:16:31 +0300

    new

--- mp/lab2/n_1.c
+++ mp/lab2/n_1.c
@@ -3,25 +3,50 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 enum status_code
 {
-    ok,
-    error_with_number_of_arguments = -1,
-    error_with_memory_allocation = -2,
-    out_of_range = -3
+    OK,
+    ERROR_WITH_NUMBER_OF_ARGUMENTS = -1,
+    ERROR_WITH_MEMORY_ALLOCATION = -2,
+    OUT_OF_RANGE = -3,
+    WRONG_ARGUMENTS = -4
 };
 
 enum flag
 {
-    error_number_arguments,
-    wrong_arguments,
-    flag_l,
-    flag_r,
-    flag_u,
-    flag_n,
-    flag_c
+    FLAG_L,
+    FLAG_R,
+    FLAG_U,
+    FLAG_N,
+    FLAG_C
 };
+
+void print_errors(int flag) 
+{
+    switch(flag) 
+    {
+        case ERROR_WITH_NUMBER_OF_ARGUMENTS:
+            printf("Invalid value\n");
+            break;
+
+        case ERROR_WITH_MEMORY_ALLOCATION:
+            printf("Error with memory allocation\n");
+            break;
+
+        case OUT_OF_RANGE:
+            printf("out of range\n");
+            break;
+
+        case WRONG_ARGUMENTS:
+            printf("This is wrong\n");
+            break;
+
+        default:
+            break;
+    }
+}
 
 enum status_code my_strlen(const char *str)
 {
@@ -32,7 +57,7 @@ enum status_code my_strlen(const char *str)
         str++;
     }
      
-    return ok;
+    return OK;
 }
 
 bool checking_unsigned_int(char *str)
@@ -48,38 +73,38 @@ bool checking_unsigned_int(char *str)
     return false;
 }
 
-int choose_flag(char* str_1, char* str_2, int argc)
+enum status_code choose_flag(char* str_1, char* str_2, int argc)
 {
-    if (argc < 3)
+    if (argc < 3 || argc > 10)
     {
-        return error_number_arguments;
+        return ERROR_WITH_NUMBER_OF_ARGUMENTS;
     }
 
     else if (argc == 3)
     {
         if (my_strlen(str_1) != 2 || str_1[0] != '-' && str_1[0] != '/') 
         {
-           return wrong_arguments; 
+           return WRONG_ARGUMENTS; 
         }
 
         if (str_1[1] == 'l')
         {
-            return flag_l;
+            return FLAG_L;
         }
 
         if (str_1[1] == 'r')
         {
-            return flag_r;
+            return FLAG_R;
         }
 
         if (str_1[1] == 'u')
         {
-            return flag_u;
+            return FLAG_U;
         }
 
         if (str_1[1] == 'n')
         {
-            return flag_n;
+            return FLAG_N;
         }
     }
 
@@ -89,17 +114,17 @@ int choose_flag(char* str_1, char* str_2, int argc)
         {
             if (my_strlen(str_2) == 10 && checking_unsigned_int(str_2) )
             {
-                return flag_c;
+                return FLAG_C;
             }
 
             else 
             {
-                return wrong_arguments;
+                return WRONG_ARGUMENTS;
             }
 
         }
     }
-    return 0;
+    return OK;
 }
 
 enum status_code reversed_function(char* string, char** new_string)
@@ -110,7 +135,7 @@ enum status_code reversed_function(char* string, char** new_string)
     
     if (*new_string == NULL)
     {
-        return error_with_memory_allocation;
+        return ERROR_WITH_MEMORY_ALLOCATION;
     }
 
     for (int i = len - 1; i >= 0; i--)
@@ -118,7 +143,7 @@ enum status_code reversed_function(char* string, char** new_string)
         (*new_string)[len - 1 - i] = string[i];
     }
 
-    return ok;
+    return OK;
 }
 
 enum status_code string_conversion(char* string, char** new_string)
@@ -127,9 +152,9 @@ enum status_code string_conversion(char* string, char** new_string)
 
     *new_string = (char*)malloc((len + 1) * sizeof(char));
 
-    if (*new_string == NULL)
+    if (!*new_string)
     {
-        return error_with_memory_allocation;
+        return ERROR_WITH_MEMORY_ALLOCATION;
     }
 
 
@@ -145,7 +170,7 @@ enum status_code string_conversion(char* string, char** new_string)
         }
     }
 
-    return ok;
+    return OK;
 }
 
 enum status_code string_digits_letters_symbols(char **new_string, char* string)
@@ -154,9 +179,9 @@ enum status_code string_digits_letters_symbols(char **new_string, char* string)
 
     *new_string = (char*)malloc((len + 1) * sizeof(char));
 
-    if (*new_string == NULL)
+    if (!*new_string)
     {
-        return error_with_memory_allocation;
+        return ERROR_WITH_MEMORY_ALLOCATION;
     }
 
     int index = 0;
@@ -190,86 +215,165 @@ enum status_code string_digits_letters_symbols(char **new_string, char* string)
 
     (*new_string)[index] = '\0';
 
-    return ok;
+    return OK;
 
 }
 
+
+// void concatination(char* str, char** result, int* len)
+// {
+//     for (int i = 0; str[i] != '\0'; ++i)
+//     {
+//         (*result)[(*len)++] = str[i];
+//     }
+
+//     (*result)[(*len)] = '\0';
+// }
+
+
+// // char max_string(char* string_1, char* string_2)
+// // {
+// //     char max_elem;
+
+// //     // Проверка, не пусты ли строки
+// //     if (string_1 == NULL && string_2 == NULL) {
+// //         return '\0';  
+// //     }
+// //     else if (string_1 == NULL) {
+// //         return *string_2;  
+// //     }
+// //     else if (string_2 == NULL) {
+// //         return *string_1;  
+// //     }
+
+// //    
+// //     if (*string_1 > *string_2) {
+// //         max_elem = *string_1;
+// //     }
+// //     else {
+// //         max_elem = *string_2;
+// //     }
+
+// //     return max_elem;
+// // }
+
+// enum status_code generating_numbers(unsigned int seed, char **numbers, int count, char **string)
+// {
+//     int len = 0;
+//     char max_string;
+
+//     if (count == 0)
+//     {
+//         return WRONG_ARGUMENTS;
+//     }
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         len = my_strlen(string[i]);
+//         // max_elem = max_string(string[i], &max_elem);
+//         if (len > max_string) 
+//         {
+//             max_string = len;
+//         }
+//     }
+
+//     max_string * count;
+
+//     *numbers = (char*)malloc((max_string + 1) * sizeof(char));
+
+//     if (!*numbers)
+//     {
+//         return ERROR_WITH_MEMORY_ALLOCATION;
+//     }
+
+//     int rand_number;
+
+//     srand(seed);
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         rand_number = rand() % count;
+//         concatination(string[rand_number], numbers, &len);
+//     }
+
+//     return OK;
+
+// }
 
 int main(int argc, char* argv[])
 {
     int flag = choose_flag(argv[1], argv[3], argc);
     int len = my_strlen(argv[2]);
-    int seed = atoi(argv[3]);
+    int len_str = argc - 3;
+    unsigned int seed = atoi(argv[3]);
     char* string = argv[2];
-    char* new_string = NULL;
+    char* new_string;
     enum status_code result_r;
     enum status_code result_u;
     enum status_code result_n;
+    enum status_code result_c;
 
     switch (flag)
     {
-        case error_number_arguments:
-            printf("Error with number of arguments\n");
-            break;
-
-        case wrong_arguments:
-            printf("Wrong arguments\n");
-            break;
-
-        case flag_l:
+        case FLAG_L:
             printf("%d\n", len);
             break;
 
-        case flag_r:
+        case FLAG_R:
             result_r = reversed_function(string, &new_string);
 
-            if (result_r == ok)
+            if (result_r == OK)
             {
                 printf("%s\n", new_string);
             }
-            else
-            {
-                printf("Error with memory allocation\n");
-            }
+
+            print_errors(result_r);
 
             free(new_string);
             break;
         
-        case flag_u:
+        case FLAG_U:
             result_u = string_conversion(string, &new_string);
 
-            if (result_u == ok)
+            if (result_u == OK)
             {
                 printf("%s\n", new_string);
             }
-            else
-            {
-                printf("Error with memory allocation\n");
-            }
+
+            print_errors(result_u);
 
             free(new_string);
             break;
         
-        case flag_n:
+        case FLAG_N:
             result_n = string_digits_letters_symbols(&new_string, string);
 
-            if (result_n == ok)
+            if (result_n == OK)
             {
                 printf("%s\n", new_string);
             }
-            else
-            {
-                printf("Error with memory allocation\n");
-            }
+
+            print_errors(result_n); 
 
             free(new_string);
 
             break;
         
-        case flag_c:
-            ////////////////////////////////////////////////////////////////////////////////////////
-            break;
-    
+        case FLAG_C:
+
+            // result_c = generating_numbers(seed, &new_string, len_str, argv + 3);
+
+            // if (result_c != OK)
+            // {
+            //     print_errors(result_c);
+            // }
+
+            // printf("%s\n", new_string);
+
+            // free(new_string);
+
+            // break;
+
     default:
         break;
     }
