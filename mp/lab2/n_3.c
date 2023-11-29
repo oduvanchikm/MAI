610d4e52b1ae55118aa31373b0aa46be974599b6
Mira <mkgubareva2005@gmail.com>
2023-11-30 00:33:34 +0300

    new

--- mp/lab2/n_3.c
+++ mp/lab2/n_3.c
@@ -21,22 +21,21 @@ void print_errors(int flag)
     }
 }
 
-void print_result(char* file_name, int line_number, int position)
+void print_result(char* name, int line_number, int position)
 {
-    printf("File %s: line is %d and position is %d\n", file_name, line_number, position);
+    printf("File %s: line is %d and position is %d\n", name, line_number, position);
 }
 
-void print_no_sub(char* file_name)
+void print_no_sub(char* name)
 {
-    printf("No sub in file: %s\n", file_name);
+    printf("No sub in file: %s\n", name);
 }
 
-status_code find_sub(FILE* file, char* sub, char* file_name)
+status_code find_sub(FILE* file, char* sub, char* name)
 {
     char line[256];
     int count = 1;
-    int success = 0;
-
+    int ok = 0;
     while(fgets(line, sizeof(line), file) != NULL)
     {
         char* position = line;
@@ -56,51 +55,46 @@ status_code find_sub(FILE* file, char* sub, char* file_name)
                 }
                 index++;
             }
-
-            if(flag)
+            if (flag)
             {
-                print_result(file_name, count, file_index);
-                success = 1;
+                print_result(name, count, file_index);
+                ok = 1;
             }
-
             position++;
             file_index++;
         }
-
         count++;
     }
-
-    if(!success)
+    if(!ok)
     {
-        print_no_sub(file_name);
+        print_no_sub(name);
     }
-
     return OK;
 }
 
 status_code get_file(char* sub, int count, ...)
 {
-    va_list ptr;
-    va_start(ptr, count);
+    va_list arg;
+    va_start(arg, count);
 
     for(int i = 0; i < count; i++)
     {
-        char* file_name = va_arg(ptr, char*);
-        FILE* file = fopen(file_name, "r");
+        char* name = va_arg(arg, char*);
+        FILE* file = fopen(name, "r");
 
         if(!file)
         {
             return ERROR_WITH_OPENING_FILE;
         }
 
-        if (find_sub(file, sub, file_name) != OK)
+        if (find_sub(file, sub, name) != OK)
         {
             fclose(file);
-            print_errors(find_sub(file, sub, file_name));
+            print_errors(find_sub(file, sub, name));
         }
         fclose(file);
     }
-    va_end(ptr);
+    va_end(arg);
     
     return OK;
 }
