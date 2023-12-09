002fe9d54a8b97670066898300d06721688e41b7
Mira <mkgubareva2005@gmail.com>
2023-12-09 11:20:36 +0300

    new

--- mp/lab3/n_5.c
+++ mp/lab3/n_5.c
@@ -2,16 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-typedef struct 
+typedef struct
 {
     unsigned int ID;
-    // char name[256];
-    // char surname[256];
-    // char group[256];
-    char *name;
-    char *surname;
-    char *group;
+    char name[256];
+    char surname[256];
+    char group[256];
     unsigned char *mark;
 } Student;
 
@@ -30,18 +28,44 @@ void print_errors(char flag)
         case ERROR_WITH_MEMORY_ALLOCATION:
             printf("Error with memory allocation\n");
             break;
-        
+
         case ERROR_WITH_OPENING_FILE:
             printf("File can't open\n");
             break;
-        
+
         case INVALID_INPUT:
             printf("Invalid input\n");
             break;
-         
+
         default:
             break;
     }
+}
+
+status_code print_student(Student student)
+{
+    printf("%u ", student.ID);
+    printf("%s ", student.name);
+    printf("%s ", student.surname);
+    printf("%s ", student.group);
+    printf("%c %c %c %c %c\n", student.mark[0], student.mark[1], student.mark[2], student.mark[3], student.mark[4]);
+    return OK;
+}
+
+status_code print_students(Student* students, int count_of_students)
+{
+    int i = 0;
+    if (students == NULL || students[i].ID == -1 || count_of_students == 0)
+    {
+        printf("Студентов нет\n");
+//        return OK;
+    }
+    while (i < count_of_students)
+    {
+        print_student(students[i]);
+        i++;
+    }
+    return OK;
 }
 
 double average_grade(Student *student)
@@ -68,76 +92,110 @@ void print_menu()
     printf("Enter:\n");
 }
 
-// status_code info_from_file(FILE *filename, Student **result, int *number_of_students)
-// {
-//     fscanf(filename, "%d", number_of_students);
-//     // *number_of_students = 9;
+bool is_mark(char mark)
+{
+    if (mark < '1' || mark > '5')
+    {
+        return false;
+    }
 
-//     *result = (Student*)malloc((*number_of_students) * sizeof(Student));
+    return true;
+}
 
-//     if (!(*result))
-//     {
-//         return ERROR_WITH_MEMORY_ALLOCATION;
-//     }
-//     int i = 0;
+int compare_by_id(const void* a, const void* b)
+{
+    unsigned id_1 = ((Student*)a)->ID;
+    unsigned id_2 = ((Student*)b)->ID;
+    if (id_1 == id_2)
+    {
+        return 0;
+    }
+    return id_1 < id_2 ? -1 : 1;
+}
 
+int compare_by_surname(const void* a, const void* b)
+{
+    char* surname_1 = ((Student*)a)->surname;
+    char* surname_2 = ((Student*)b)->surname;
+    return strcmp(surname_1, surname_2);
+}
 
-//     while ((fscanf(filename, "%u %s %s %s", &((*result)[i].ID), ((*result)[i].name), ((*result)[i].surname), ((*result)[i].group))) != EOF)
-//     {
-//         (*result)[i].mark = (unsigned char*)malloc(sizeof(unsigned char) * 5);
+int compare_by_name(const void* a, const void* b)
+{
+    char* name_1 = ((Student*)a)->name;
+    char* name_2 = ((Student*)b)->name;
+    return strcmp(name_1, name_2);
+}
 
-//         if (!(*result)[i].mark)
-//         {
-//             for (int j = 0; j < i; j++)
-//             {
-//                 free((*result)[j].mark);
-//             }
-//             free(*result);
-//             return ERROR_WITH_MEMORY_ALLOCATION;
-//         }
+int compare_by_group(const void* a, const void* b)
+{
+    char* group_1 = ((Student*)a)->group;
+    char* group_2 = ((Student*)b)->group;
+    return strcmp(group_1, group_2);
+}
 
-//         int num_of_marks_read = 0;
+Student search_by_id(unsigned int id, Student* result)
+{
+    int i = 0;
+    while (result[i].ID != -1)
+    {
+        if (result[i].ID == id)
+        {
+            return result[i];
+        }
+        i++;
+    }
+    Student student;
+    student.ID = -1;
+    return student;
+}
 
-//         for (int j = 0; j < 5; j++)
-//         {
-//             if (fscanf(filename, "%c", &((*result)[i].mark[j])) != 1)
-//             {
-//                 free((*result)[i].mark);
-//                 for (int k = 0; k < i; k++)
-//                 {
-//                     free((*result)[k].mark);
-//                 }
-//                 free(*result);
-//                 // printf("err\n");
-//                 return INVALID_INPUT;
-//             }
+//Student* search_by_name(Student* students, int *amount_students, const char* name)
+//{
+//    for (int i = 0; i < amount_students; i++)
+//    {
+//        if (strcmp(students[i].name, name) == 0)
+//        {
+//            return &students[i];
+//        }
+//    }
+//    return NULL;
+//}
 
-//             if (!isdigit((*result)[i].mark[j]))
-//             {
-//                 free((*result)[i].mark);
-//                 for (int k = 0; k < i; k++)
-//                 {
-//                     free((*result)[k].mark);
-//                 }
-//                 free(*result);
-//                 printf("err\n");
-//                 return INVALID_INPUT;
-//             }
-//             num_of_marks_read++;
-//         }
-//         for (int j = num_of_marks_read; j < 5; j++)
-//         {
-//             (*result)[i].mark[j] = '2';
-//         }
-//         i++;
-//     }
-//     return OK;
-// }
+status_code search_by_surname(Student* students, int amount_students, const char* surname, Student **result)
+{
+    for (int i = 0; i < amount_students; i++)
+    {
+        if (strcmp(students[i].surname, surname) == 0)
+        {
+            *result = &students[i];
+            return OK;
+        }
+    }
+    return INVALID_INPUT;
+}
+
+status_code search_by_group(Student* students, int count, char* group, Student** result)
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (strcmp(students[i].group, group) == 0)
+        {
+            *result = &students[i];
+            return OK;
+        }
+    }
+    *result = NULL;
+    return INVALID_INPUT;
+}
 
 status_code info_from_file(FILE *filename, Student **result, int *number_of_students)
 {
-    *result = (Student*)malloc(sizeof(Student));
-    if (!(*result)) {
+    *number_of_students = 9;
+
+    *result = (Student*)malloc(sizeof(Student) * (*number_of_students + 1));
+    if (!(*result))
+    {
         return ERROR_WITH_MEMORY_ALLOCATION;
     }
 
@@ -145,9 +203,9 @@ status_code info_from_file(FILE *filename, Student **result, int *number_of_stud
     while (fscanf(filename, "%d %s %s %s", &((*result)[i].ID), ((*result)[i].name), ((*result)[i].surname), ((*result)[i].group)) == 4)
     {
         (*result)[i].mark = (unsigned char*)malloc(sizeof(unsigned char) * 5);
-        if (!(*result)[i].mark) 
+        if (!(*result)[i].mark)
         {
-            for (int j = 0; j <= i; j++) 
+            for (int j = 0; j <= i; j++)
             {
                 free((*result)[j].mark);
             }
@@ -155,11 +213,16 @@ status_code info_from_file(FILE *filename, Student **result, int *number_of_stud
             return ERROR_WITH_MEMORY_ALLOCATION;
         }
 
+//        if (!(isdigit((*result)[i].ID)) || !(isalpha((*result)[i].name)) || !(isalpha((*result)[i].surname)))
+//        {
+//            return INVALID_INPUT;
+//        }
+
         for (int j = 0; j < 5; j++)
         {
-            if (fscanf(filename, " %c", &((*result)[i].mark[j])) != 1 || !isdigit((*result)[i].mark[j]))
+            if (fscanf(filename, " %c", &((*result)[i].mark[j])) != 1 || !is_mark((*result)[i].mark[j]))
             {
-                for (int k = 0; k <= i; k++) 
+                for (int k = 0; k <= i; k++)
                 {
                     free((*result)[k].mark);
                 }
@@ -168,10 +231,10 @@ status_code info_from_file(FILE *filename, Student **result, int *number_of_stud
             }
         }
         i++;
-        *result = (Student*)realloc(*result, (i+1) * sizeof(Student));
-        if (!(*result)) 
+        *result = (Student*)realloc(*result, (i + 1) * sizeof(Student));
+        if (!(*result))
         {
-            for (int j = 0; j < i; j++) 
+            for (int j = 0; j < i; j++)
             {
                 free((*result)[j].mark);
             }
@@ -179,7 +242,6 @@ status_code info_from_file(FILE *filename, Student **result, int *number_of_stud
             return ERROR_WITH_MEMORY_ALLOCATION;
         }
     }
-
     *number_of_students = i;
     return OK;
 }
@@ -202,6 +264,13 @@ int main(int argc, char *argv[])
         fclose(output_file);
         return 0;
     }
+
+    if (strcmp(argv[1], argv[2]) == 0)
+    {
+        printf("I must enter different files\n");
+        return 0;
+    }
+
     Student *result;
     int count_of_student;
 
@@ -210,21 +279,143 @@ int main(int argc, char *argv[])
     if (st_info != OK)
     {
         print_errors(st_info);
+        fclose(input_file);
+        fclose(output_file);
+        return 0;
     }
 
-    else
+    int c;
+    print_menu();
+    if (scanf("%d", &c) != 1)
     {
-        for (int i = 0; i < count_of_student; i++)
+        printf("Wrong number of option\nPlease, enter again\n");
+    }
+    int flag_sort;
+    int flag_search;
+    Student *students;
+    Student* st = NULL;
+    Student *stud;
+
+    while (c > 0 && c <= 4)
+    {
+        switch(c)
         {
-            fprintf(output_file, "Id: %u\nName: %s\nSurname: %s\nGroup: %s\nMarks: ", result[i].ID, result[i].name, result[i].surname, result[i].group);
-            for (int j = 0; j < 5; j++)
-            {
-                fprintf(output_file, "%c ", result[i].mark[j]);
-            }
-            fprintf(output_file, "\n");
+            case 1:
+                printf("\tFind a student\n");
+                printf("You can search students by\n1. ID\n2. Name\n3. Surname\n4. Group\n");
+                if (scanf("%d", &flag_search) != 1)
+                {
+                    printf("Wrong number of search\nPlease, enter again\n");
+                }
+                switch (flag_search)
+                {
+                    case 1:
+                        printf("Enter ID:\n");
+                        int id;
+                        if (scanf("%d", &id) != 1 || id < 1 || id > 5)
+                        {
+                            printf("Error with enter ID\n");
+                        }
+                        print_student(search_by_id(id, result));
+                        break;
+
+//                    case 2:
+//                        printf("Enter name:\n");
+//                        char name[50];
+//                        scanf("%s", name);
+//                        Student* search = search_by_name(stud, count_of_student, name);
+//                        print_students(search, count_of_student);
+//
+//                        break;
+//                    case 3:
+//                        printf("Enter surname:\n");
+//                        char surname[50];
+//                        scanf("%s", surname);
+//
+//                        if (!isalpha(surname[0]))
+//                        {
+//                            printf("Error with enter name\n");
+//                        }
+//                        else
+//                        {
+//                            status_code result_sur = search_by_surname(students, count_of_student, name, &st);
+//                            if (result_sur == OK && st != NULL)
+//                            {
+//                                print_student(*st);
+//                            }
+//                            else
+//                            {
+//                                printf("Student not found\n");
+//                            }
+//                        }
+//                        break;
+//
+//                    case 4:
+//                        printf("Enter group:\n");
+//                        char group[50];
+//                        scanf("%s", group);
+//                        status_code result_gr = search_by_group(students, count_of_student, name, &st);
+//                        if (result_gr == OK && st != NULL)
+//                        {
+//                            print_student(*st);
+//                        }
+//                        else
+//                        {
+//                            printf("Student not found\n");
+//                        }
+//                        break;
+//                    default:
+//                        break;
+                }
+                break;
+
+            case 2:
+                printf("\tSort students\n");
+                printf("You can sort students by\n1. ID\n2. Name\n3. Surname\n4. Group\n");
+                scanf("%d", &flag_sort);
+                if (flag_sort < 1 || flag_sort > 5)
+                {
+                    printf("Wrong number of sort\nPlease, enter again\n");
+                }
+                if (flag_sort == 1)
+                {
+                    qsort(students, count_of_student, sizeof(Student), compare_by_id);
+                }
+
+                else if (flag_sort == 2)
+                {
+                    qsort(students, count_of_student, sizeof(Student), compare_by_name);
+                }
+
+                else if (flag_sort == 3)
+                {
+                    qsort(students, count_of_student, sizeof(Student), compare_by_surname);
+                }
+
+                else if (flag_sort == 4)
+                {
+                    qsort(students, count_of_student, sizeof(Student), compare_by_group);
+                }
+
+                for (int i = 0; i < count_of_student; i++){
+                    printf("\tId: %d, name: %s, surname: %s, group: %s, grades: %c %c %c %c %c\n",
+                           students[i].ID, students[i].name, students[i].surname, students[i].group,
+                           students[i].mark[0], students[i].mark[1], students[i].mark[2],
+                           students[i].mark[3], students[i].mark[4]);
+                }
+
+            case 3:
+                printf("erjr\n");
+                break;
+
+            case 4:
+                printf("fgjrhg\n");
+                break;
+
+            default:
+                break;
         }
     }
-
     fclose(input_file);
     fclose(output_file);
     return 0;
