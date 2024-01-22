2e62f183353bf2b8e49e1a95b8c2e1d3c78ff304
Mira <mkgubareva2005@gmail.com>
2024-01-23 00:08:55 +0300

    new

--- mp/lab4/n_5.c
+++ mp/lab4/n_5.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <stdbool.h>
-#include <math.h>
+#include <limits.h>
 #include <ctype.h>
 
 typedef enum
@@ -13,7 +12,10 @@ typedef enum
     INVALID_INPUT,
     ERROR_WITH_COUNT_OF_ARGUMENTS,
     ERROR_WITH_OPENING_FILE,
-    EMPTY
+    EMPTY,
+    NEGATIVE_DIGIT,
+    WRONG_COUNT_OF_BRACKETS,
+    OVERFLOW
 
 } status_code;
 
@@ -36,6 +38,18 @@ void print_errors(int flag)
 
         case ERROR_WITH_OPENING_FILE:
             printf("Error with opening file\n");
+            break;
+
+        case NEGATIVE_DIGIT:
+            printf("It's a negativ digit\n");
+            break;
+
+        case WRONG_COUNT_OF_BRACKETS:
+            printf("Wrong count of brackets\n");
+            break;
+
+        case OVERFLOW:
+            printf("It's very big digit\n");
             break;
         
         default:
@@ -64,10 +78,9 @@ void initialize(Stack* stack)
 void push(Stack* stack, int data) 
 {
     Node* new_node = (Node*)malloc(sizeof(Node));
-
     if (!new_node)
     {
-        ERROR_WITH_MEMORY_ALLOCATION;
+        return;
     }
 
     new_node->data = data;
@@ -75,19 +88,29 @@ void push(Stack* stack, int data)
     stack->top = new_node;
 }
 
-int pop(Stack* stack) 
+char pop(Stack** stack)
+{
+    if (*stack == NULL)
+    {
+        return INVALID_INPUT;
+    }
+
+    Node* out = (*stack)->top;
+    char value = out->data;
+    (*stack)->top = (*stack)->top->next;
+    free(out);
+    return value;
+}
+
+
+char peek(Stack* stack)
 {
     if (stack->top == NULL) 
     {
         return EMPTY;
     }
-    int data = stack->top->data;
-    Node* temp = stack->top;
-    stack->top = stack->top->next;
 
-    free(temp);
-
-    return data;
+    return stack->top->data;
 }
 
 int is_empty(Stack* stack) 
@@ -95,293 +118,380 @@ int is_empty(Stack* stack)
     return stack->top == NULL;
 }
 
-bool is_operator(char c) 
+bool is_operator(const char c)
 {
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%');
+    return ((c == '+') || (c == '-') || (c == '*') || (c == '/') || (c == '^') || (c == '%') || (c == '~'));
 }
 
-int get_priority(char operator) 
+int get_priority(const char operator)
 {
-    if (operator == '^') 
+    switch (operator)
     {
-        return 3;
+        case '(':
+        case ')':
+            return 1;
+
+        case '+':
+        case '-':
+            return 2;
+
+        case '*':
+        case '/':
+        case '%':
+        case '~':
+            return 3;
+
+        case '^':
+            return 4;
+
+        default:
+            return 5;
     }
-    else if (operator == '*' || operator == '/' || operator == '%') 
-    {
-        return 2;
-    }
-    else if (operator == '+' || operator == '-') 
+}
+
+int my_pow(int base, int power)
+{
+    int result = 0;
+
+    if (power == 0)
     {
         return 1;
     }
-    else 
+
+    else if (power == 1)
+    {
+        return base;
+    }
+
+    else
+    {
+        if (power % 2 == 0)
+        {
+            result = my_pow(base, power / 2);
+            return result * result;
+        }
+        else
+        {
+            return my_pow(base, power - 1) * base;
+        }
+    }
+}
+
+int my_strlen(const char* string)
+{
+    if (!(*string))
     {
         return 0;
     }
+    int count = 0;
+
+    while (*string)
+    {
+        string++;
+        count++;
+    }
+    return count;
 }
 
-// char* infix_to_postfix(char* expression) // Shunting Yard algorithm
-// {
-//     Stack stack;
-//     stack.top = NULL;
-
-//     int len = strlen(expression);
-
-//     char *postfix = malloc(len * 2);
-
-//     if (!postfix)
-//     {
-//         return;
-//     }
-
-//     int digit = 0;
-//     for (int i = 0; expression[i] != '\0'; i++)
-//     {
-//         char c = expression[i];
-
-//         if (c >= '0' && c <= '9')
-//         {
-//             postfix[digit++] = c;
-//         }
-
-//         else if (is_operator(c))
-//         {
-//             if (is_empty(&stack))
-//             {
-//                 push(&stack, c);
-//             }
-//             else
-//             {
-//                 char temp = pop(&stack);
-//                 if (get_priority(temp) <= get_priority(c))
-//                 {
-//                     push(&stack, c);
-//                     push(&stack, temp); 
-//                 }
-//                 else
-//                 {
-//                     push(&stack, temp);
-//                     push(&stack, c);
-//                 }
-//             }
-//         }
-
-//         else if (c == '(')
-//         {
-//             push(&stack, c);
-//         }
-//         else if (c == ')')
-//         {
-//             char temp = pop(&stack);
-
-//             while (!is_empty(&stack) && temp != '(') 
-//             {
-//                 postfix[digit++] = pop(&stack);
-//             }
-
-//             if (!is_empty(&stack) && temp != '(') 
-//             {
-//                 free(postfix);
-//                 return NULL;
-//             } 
-//             else 
-//             {
-//                 pop(&stack);
-//             }
-//         }
-//     }
-    
-//     while (!is_empty(&stack))
-//     {
-//         postfix[digit++] = pop(&stack);
-//     }
-    
-//     postfix[digit] = '\0';
-//     return postfix;
-// }
-
-char* infix_to_postfix(char* expression) 
+status_code infix_to_postfix(const char* infix, char** postfix)
 {
-    Stack stack;
-    initialize(&stack);
-
-    int len = strlen(expression);
-
-    char *postfix = (char*)malloc(len + 1);
-
-    if (!postfix)
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    if (!stack)
     {
-        return NULL;
+        return ERROR_WITH_MEMORY_ALLOCATION;
     }
 
-    int digit = 0;
+    initialize(stack);
 
-    for (int i = 0; expression[i] != '\0'; i++)
+    int len = my_strlen(infix);
+    *postfix = (char*)malloc((len + 1) * sizeof(char));
+    if (!(*postfix))
     {
-        char c = expression[i];
+        return ERROR_WITH_MEMORY_ALLOCATION;
+    }
 
-        if (c >= '0' && c <= '9')
+    int postfix_size = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (isspace((infix[i])))
         {
-            postfix[digit++] = c;
+            continue;
         }
 
-        else if (is_operator(c))
+        else if (infix[i] == '-' && (i == 0 || infix[i - 1] == '('))
         {
-            if (is_empty(&stack))
-            {
-                push(&stack, c);
-            }
-            else
-            {
-                char temp = pop(&stack);
-                if (get_priority(temp) <= get_priority(c))
-                {
-                    push(&stack, c);
-                    push(&stack, temp); 
-                }
-                else
-                {
-                    push(&stack, temp);
-                    push(&stack, c);
-                }
-            }
+            (*postfix)[postfix_size] = '~';
+            postfix_size++;
         }
 
-        else if (c == '(')
+        else if (isdigit((infix[i])))
         {
-            push(&stack, c);
+            while (isdigit(infix[i]))
+            {
+                (*postfix)[postfix_size] = infix[i];
+                postfix_size++;
+                i++;
+            }
+            (*postfix)[postfix_size] = ' ';
+            postfix_size++;
+            i--;
         }
 
-        else if (c == ')')
+        else if (infix[i] == '(')
         {
-            char temp = pop(&stack);
+            push(stack, infix[i]);
+        }
 
-            while (!is_empty(&stack) && temp != '(') 
+        else if (infix[i] == ')')
+        {
+            while (!is_empty(stack) && peek(stack) != '(')
             {
-                postfix[digit++] = pop(&stack);
+                (*postfix)[postfix_size] = pop(&stack);
+                postfix_size++;
+
+                (*postfix)[postfix_size] = ' ';
+                postfix_size++;
             }
 
-            if (!is_empty(&stack) && temp != '(') 
-            {
-                free(postfix);
-                return NULL;
-            } 
-            else 
+            if (!is_empty(stack))
             {
                 pop(&stack);
             }
+            else
+            {
+                return WRONG_COUNT_OF_BRACKETS;
+            }
+        }
+
+        else if (is_operator(infix[i]))
+        {
+            while (!is_empty(stack) && get_priority(peek(stack)) >= get_priority(infix[i]))
+            {
+                (*postfix)[postfix_size] = pop(&stack);
+                postfix_size++;
+                (*postfix)[postfix_size] = ' ';
+                postfix_size++;
+            }
+            push(stack, infix[i]);
+        }
+        else
+        {
+            return INVALID_INPUT;
         }
     }
+    while (!is_empty(stack))
+    {
+        if (peek(stack) == '(' || peek(stack) == ')')
+        {
+            return INVALID_INPUT;
+        }
+        (*postfix)[postfix_size] = pop(&stack);
+        postfix_size++;
+
+        (*postfix)[postfix_size] = ' ';
+        postfix_size++;
+    }
+    (*postfix)[postfix_size] = '\0';
+
+    return OK;
 }
 
-
-int check_balance(char* expression) 
+status_code solve_expression(char* postfix, int* result)
 {
-    Stack stack;
-    stack.top = NULL;
+    int number = 0;
+    int number_1 = 0;
+    int number_2 = 0;
 
-    int len = strlen(expression);
+    int len = my_strlen(postfix);
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    if (!stack)
+    {
+        return ERROR_WITH_MEMORY_ALLOCATION;
+    }
+
+    initialize(stack);
+
     for (int i = 0; i < len; i++)
     {
-        char c = expression[i];
-        if (c == '(')
+        if (isdigit(postfix[i]))
         {
-            push(&stack, c);
+            number = number * 10 + (postfix[i] - '0');
         }
-        else if (c == ')')
+        else if (postfix[i] == ' ' && isdigit(postfix[i - 1]))
         {
-            if (is_empty(&stack) || pop(&stack) != '(')
+            push(stack, number);
+            number = 0;
+        }
+        else if (postfix[i] == '~')
+        {
+            number = pop(&stack);
+            push(stack, -number);
+        }
+        else if (postfix[i] == ' ')
+        {
+            continue;
+        }
+        else
+        {
+            number_2 = pop(&stack);
+            number_1 = pop(&stack);
+            if ((number_1 >= INT_MAX && number_2 >= INT_MAX) || (number_1 <= INT_MIN && number_2 <= INT_MIN))
             {
-                return 0;
+                return OVERFLOW;
+            }
+            switch(postfix[i])
+            {
+                case '+':
+                    push(stack, number_1 + number_2);
+                    break;
+                case '-':
+                    push(stack, number_1 - number_2);
+                    break;
+                case '*':
+                    push(stack, number_1 * number_2);
+                    break;
+                case '/':
+                    if (number_2 != 0)
+                    {
+                        push(stack, number_1 / number_2);
+                    }
+                    else
+                    {
+                        return NEGATIVE_DIGIT;
+                    }
+                    break;
+                case '%':
+                    if (number_2 != 0)
+                    {
+                        push(stack, number_1 % number_2);
+                    }
+                    else
+                    {
+                        return NEGATIVE_DIGIT;
+                    }
+                    break;
+                case '^':
+                    push(stack, my_pow(number_1, number_2));
+                    break;
+                default:
+                    return INVALID_INPUT;
             }
         }
     }
-    
-    return is_empty(&stack);
+    *result = stack->top->data;
+    return OK;
 }
 
-int solve_expression(char* expression) 
+status_code read_string(FILE* file, char** string)
 {
-    Stack stack;
-    int i = 0;
-    while (expression[i]) 
+    if (!file)
     {
-        if (isdigit(expression[i])) 
-        {
-            push(&stack, expression[i] - '0');
-        }
-        else 
-        {
-            double operator1 = pop(&stack);
-            double operator2 = pop(&stack);
-            switch (expression[i]) 
-            {
-                case '+': 
-                    push(&stack, operator1 + operator2);
-                    break;
-
-                case '-': 
-                    push(&stack, operator1 - operator2);
-                    break;
-
-                case '*': 
-                    push(&stack, operator1 * operator2);
-                    break;
-
-                case '/': 
-                    push(&stack, operator1 / operator2);
-                    break;
-
-                case '^': 
-                    push(&stack, pow(operator1, operator2));
-                    break;
-            }
-        }
-        i++;
+        return ERROR_WITH_OPENING_FILE;
     }
-    return pop(&stack);
+    int index = 0;
+    int capacity = 10;
+    char symbol = fgetc(file);
+    *string = (char*)malloc(sizeof(char*) * capacity);
+    if (!(*string))
+    {
+        return ERROR_WITH_MEMORY_ALLOCATION;
+    }
+
+    while (symbol != '\n' && symbol != EOF)
+    {
+        if (index == capacity - 1)
+        {
+            capacity *= 2;
+            char* tmp = realloc(*string, capacity);
+            if (!tmp)
+            {
+                free(*string);
+                return ERROR_WITH_MEMORY_ALLOCATION;
+            }
+            *string = tmp;
+        }
+        (*string)[index] = (char)symbol;
+        index++;
+        symbol = fgetc(file);
+    }
+
+    (*string)[index] = '\0';
+    return OK;
 }
 
-int main(int argc, char* argv[]) 
+status_code file_works(char* argv[], int argc)
 {
+    char* string = NULL;
+    char* postfix = NULL;
+    int digit;
+
     if (argc < 2)
     {
-        printf("Error with number of arguments\n");
-        exit(1);
+        return INVALID_INPUT;
     }
 
-    for (int i = 1; i < argc; i++) 
+    for (int i = 1; i < argc; i++)
     {
         FILE* file = fopen(argv[i], "r");
-        if (!file) 
+        if (!file)
         {
-            printf("error\n");
-            exit(1);
+            return ERROR_WITH_OPENING_FILE;
         }
-        char line[1000];
 
-        while (fgets(line, sizeof(line), file)) 
+        if (read_string(file, &string) != OK)
         {
-            printf("Infix expression: %s\n", line);
-            char expression[1000];
-            strcpy(expression, line);
-
-            if (check_balance(expression)) 
-            {
-                char* postfix_expression = infix_to_postfix(expression);
-                printf("Postfix expression: %s\n", postfix_expression);
-
-                int value = solve_expression(postfix_expression);
-                printf("Result: %d\n", value);
-                
-                free(postfix_expression);
-            }
-            else 
-            {
-                printf("ERROR\n");
-            }
+            fclose(file);
+            print_errors(read_string(file, &string));
         }
+
+        if (!string)
+        {
+            fclose(file);
+            return INVALID_INPUT;
+        }
+
+        if (infix_to_postfix(string, &postfix) != OK)
+        {
+            fclose(file);
+            free(string);
+            print_errors(infix_to_postfix(string, &postfix));
+        }
+
+        printf("Postfix expression: %s\n", postfix);
+
+        if (solve_expression(postfix, &digit) != OK)
+        {
+            fclose(file);
+            free(postfix);
+            free(string);
+            print_errors(solve_expression(postfix, &digit));
+        }
+
+        printf("Result of expression is %d\n", digit);
+
+        free(postfix);
+        free(string);
+
         fclose(file);
+    }
+
+    return OK;
+}
+
+
+int main(int argc, char* argv[])
+{
+    status_code st = file_works(argv, argc);
+    if (st == OK)
+    {
+        printf("g\n");
+    }
+
+    else
+    {
+        print_errors(st);
     }
     return 0;
 }
+
